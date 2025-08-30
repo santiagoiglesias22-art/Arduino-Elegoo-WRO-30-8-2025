6dbde257106a7af2822657e437e3b9c20f4e08a9
santiagoiglesias22-art <santiagoiglesias22@agustiniano.edu.gt>
2025-08-30 08:47:55 -0600

    Add files via upload

---
+++ DeviceDriverSet_ULTRASONIC (1).cpp
@@ -0,0 +1,24 @@
+#include "DeviceDriverSet_ULTRASONIC.h"
+
+void DeviceDriverSet_ULTRASONIC::begin(uint8_t trigPin, uint8_t echoPin) {
+  _trigPin = trigPin;
+  _echoPin = echoPin;
+  pinMode(_trigPin, OUTPUT);
+  pinMode(_echoPin, INPUT);
+}
+
+long DeviceDriverSet_ULTRASONIC::medirDistancia() {
+  digitalWrite(_trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(_trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(_trigPin, LOW);
+
+  long duracion = pulseIn(_echoPin, HIGH, 30000UL); // timeout 30ms
+  if (duracion == 0) return 0; // fuera de rango
+  long distancia = duracion * 0.034 / 2; // cm
+  return distancia;
+}
+
+
---
+++ DeviceDriverSet_ULTRASONIC (1).h
@@ -0,0 +1,18 @@
+#ifndef _DeviceDriverSet_ULTRASONIC_H_
+#define _DeviceDriverSet_ULTRASONIC_H_
+
+#include <Arduino.h>
+
+class DeviceDriverSet_ULTRASONIC {
+  public:
+    void begin(uint8_t trigPin, uint8_t echoPin);
+    long medirDistancia();
+
+  private:
+    uint8_t _trigPin;
+    uint8_t _echoPin;
+};
+
+#endif
+
+
